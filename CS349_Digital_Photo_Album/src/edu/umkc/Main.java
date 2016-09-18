a06f88f74e5c69dbecafe41b3cc7115066c85ee4
Hank.Fridell <hfridell@mail.umkc.edu>
2016-09-18 15:39:42 -0500

    Added Photo Album

---
+++ CS349_Digital_Photo_Album/src/edu/umkc/Main.java
@@ -0,0 +1,16 @@
+package edu.umkc;
+
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+        //create and show GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+
+                MainLayout.createAndShowGUI();
+            }
+        });
+    }
+}
